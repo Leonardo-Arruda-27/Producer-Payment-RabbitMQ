fc90a0643d4a7f90a087867f0a206b71529f16f1
Leonardo-Arruda-27 <leo.5dgoncalves@gmail.com>
2024-06-18 22:22:58 -0300

    Producer

---
+++ OrderPublisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace Fila_RabbitMQ
+{
+    public class OrderPublisher
+    {
+        private readonly IModel _channel;
+
+        public OrderPublisher(IModel channel)
+        {
+            _channel = channel;
+            channel.QueueDeclare(queue: "orderQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
+
+        public void PublishOrder(dynamic order)
+        {
+            string message = JsonSerializer.Serialize(order);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: "",
+                                 routingKey: "orderQueue",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using Fila_RabbitMQ;
+using RabbitMQ.Client;
+
+class Publisher
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+
+            var orderPublisher = new OrderPublisher(channel);
+
+            while (true)
+            {
+                var order = new
+                {
+                    userId = "12345",
+                    userName = "John Doe",
+                    email = "john.doe@example.com",
+                    order = new
+                    {
+                        orderId = "67890",
+                        totalAmount = 150.00,
+                        currency = "USD",
+                        items = new[]
+                        {
+                            new { itemId = "1", itemName = "Product 1", quantity = 2, price = 50.00 },
+                            new { itemId = "2", itemName = "Product 2", quantity = 1, price = 50.00 }
+                        }
+                    },
+                    payment = new
+                    {
+                        cardNumber = "4111111111111111",
+                        expiryDate = "12/23",
+                        cvv = "123"
+                    }
+                };
+
+                orderPublisher.PublishOrder(order);
+
+                // Espera 5 segundos antes de enviar a próxima mensagem
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
